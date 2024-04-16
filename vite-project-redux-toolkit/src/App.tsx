d6a95fd75f3d2a6fec759eaebd548cbfb96fc37f
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-16 13:58:07 +0500

    Mission accomplished

--- vite-project-redux-toolkit/src/App.tsx
+++ vite-project-redux-toolkit/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { increment, decrement } from './redux/slices/counter/counter';
+import { useAppDispatch, useAppSelector } from './redux/hooks/hook'
+import MyComp from './MyComponent/MyComp';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => (state.counter));
+  const dispatch = useAppDispatch()
 
   return (
     <>
@@ -16,7 +21,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Count is: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>{" "}
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <MyComp/>
+      {/* <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,9 +36,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
 export default App
