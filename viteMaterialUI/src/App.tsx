4034b5a02fdb35687ae9144ba6822884388119bd
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-23 16:25:11 +0500

    app has some changes

--- viteMaterialUI/src/App.tsx
+++ viteMaterialUI/src/App.tsx
@@ -1,15 +1,17 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Button from './Button/Button'
+// import Button from './Button/Button'
 import './App.css'
+import BreakPoints from './BreakPoints/BreakPoints'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Button/>
+      {/* <Button/> */}
+      <BreakPoints/>
     </div>
   )
 }
