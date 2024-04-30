02df1b50f731494fedd1caafe97638d30818e687
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-30 01:31:30 -0700

    styling app.js

--- material-ui-demo/src/App.jsx
+++ material-ui-demo/src/App.jsx
@@ -1,10 +1,119 @@
 import React from "react";
-import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@mui/material";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+  CardContent,
+} from "@mui/material";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+
 const App = () => {
   return (
-    <div>
-      <Typography variant="h1">Hello, World!</Typography>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon
+            style={{
+              marginRight: "20px",
+            }}
+          />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div
+          style={{
+            backgroundColor: "#fcfcfc",
+            paddingTop: "64px",
+            paddingBottom: "64px",
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello everyone! This is a photo album and I'm trying to mae this
+              sentence as long as possible so we can see how this will look like
+              on web document page.
+            </Typography>
+            <div
+              style={{
+                marginTop: "40px",
+              }}
+            >
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container maxWidth="md" style={{ padding: "20px" }}>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <CardMedia
+                  style={{ paddingTop: "56.25%" }}
+                  image="https://source.unsplash.com/random"
+                  title="image title"
+                >
+                  <CardContent style={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Exit
+                    </Button>
+                  </CardActions>
+                </CardMedia>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 };
 
